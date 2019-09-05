a774687018254915dde00f797a3db4c1f93189fc
“mayinghan” <ma.yingha@ufl.edu>
2019-09-05 17:26:50 -0400

    add convertToToken

--- convert_token.py
+++ convert_token.py
@@ -1,16 +1,26 @@
 import os
 import sys
+import string
+import re
 
 from pytorch_transformers import BertTokenizer
 
-def read_corpus():
-  pass
-
-def tokenize():
+def convert_corpus_to_tokens(filepath):
+  with open('tokened_mimic.txt', 'w') as w:
+    with open(filepath, 'r') as f:
+      for line in f:
+        token_list = tokenize(line)
+        new_token_sentence = ' '.join(token_list) + '\n'
+        w.write(new_token_sentence)
+      
+        
+    
+def tokenize(word) -> list:
   tokenizer = BertTokenizer.from_pretrained('bert-base-cased'
                 , do_lower_case=False)
 
-  print(tokenizer.tokenize('i love u'))
+  return tokenizer.tokenize(word)
+
 
 if __name__ == "__main__":
-    tokenize()
+    convert_corpus_to_tokens(os.path.join('./mimiciii_notes_new.sent.lowered.all0.txt'))
